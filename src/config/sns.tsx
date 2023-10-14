0475f41fe46649163bfd55a16e5535dbde5724ea
Maltonn <ymatsubayashi09@outlook.jp>
2023-10-15 03:04:54 +0900

    change README

--- src/config/sns.tsx
+++ src/config/sns.tsx
@@ -1,18 +1,18 @@
 import { SiGithub, SiTwitter, SiZenn } from 'react-icons/si';
 
 export const sns = [
+  // {
+  //   href: 'https://zenn.dev',
+  //   icon: <SiZenn size={20} />,
+  //   label: 'Zenn',
+  // },
   {
-    href: 'https://zenn.dev',
-    icon: <SiZenn size={20} />,
-    label: 'Zenn',
-  },
-  {
-    href: 'https://github.com',
+    href: 'https://github.com/maltonn',
     icon: <SiGithub size={20} />,
     label: 'Github',
   },
   {
-    href: 'https://twitter.com',
+    href: 'https://twitter.com/maltonn_',
     icon: <SiTwitter size={20} />,
     label: 'Twitter',
   },
