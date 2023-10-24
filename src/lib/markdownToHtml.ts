3e989213fac4e2158f62da9cb075aa067e601467
Malton <ymatsubayashi09@outlook.jp>
2023-10-25 00:50:14 +0900

    tmp

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,11 +1,11 @@
 import rehypePrism from '@mapbox/rehype-prism';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeKatex from 'rehype-katex'
 import rehypeSlug from 'rehype-slug';
 import rehypeStringify from 'rehype-stringify';
+import remarkMath from 'remark-math'
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
-import rehypeKatex from 'rehype-katex'
-import remarkMath from 'remark-math'
 import { unified } from 'unified';
 
 export default async function markdownToHtml(markdown: string) {
