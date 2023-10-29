1f88b03c7950563fde615263ac46f71e1fd0bcb6
Maltonn <ymatsubayashi09@outlook.jp>
2023-10-29 19:28:42 +0900

    update

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -6,6 +6,8 @@ import rehypeStringify from 'rehype-stringify';
 import remarkMath from 'remark-math'
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
+import remarkImages from 'remark-images'
+
 import { unified } from 'unified';
 
 export default async function markdownToHtml(markdown: string) {
@@ -13,6 +15,7 @@ export default async function markdownToHtml(markdown: string) {
     .use(remarkParse)
     .use(remarkMath)
     .use(remarkRehype)
+    .use(remarkImages)
     .use(rehypeKatex)
     .use(rehypePrism)
     .use(rehypeSlug)
